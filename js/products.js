92ead24e249cdc2f700967f9be9450d3fb25c6c6
Robert Kirui <kirugik79@gmail.com>
2022-03-19 20:55:40 +0300

    Add products.js file that contains all the objects

---
+++ js/products.js
@@ -0,0 +1,22 @@
+const small = {
+    id: 0,
+    name: "Small Pizza",
+    price: 499,
+    crusts: ["Neapolitan crust","New York crust", "Chicago crust", "Pan Pizza crust", "Sicilian crust", "St. Louis crust"],
+    toppings: ["Roasted tomatoes", "Pickled red onions"]
+},
+
+const medium = {
+    id: 1,
+    name: "Medium Pizza",
+    price: 799,
+    crusts: ["Neapolitan crust","New York crust", "Chicago crust", "Pan Pizza crust", "Sicilian crust", "St. Louis crust"],
+    toppings:["Roasted tomatoes", "Pickled red onions"]
+},
+const large = {
+    id: 2,
+    name: "Large Pizza",
+    price: 1199,
+    crusts: ["Neapolitan crust","New York crust", "Chicago crust", "Pan Pizza crust", "Sicilian crust", "St. Louis crust"],
+    toppings: ["Roasted tomatoes", "Pickled red onions"]
+};
